f5ab897da6b3a6cec3d6ab75b224767951b70271
dantevergil2131 <rkorandy07@gmail.com>
2021-04-28 10:27:22 +0530

    task updated

--- src/org/sel/Singleton2.java
+++ src/org/sel/Singleton2.java
@@ -1,10 +1,13 @@
 package org.sel;
 
 public class Singleton2 {
+	
+	
 public static void main(String[] args) {
 	Singleton w=Singleton.getId();
 	w.getName();
 	System.out.println(System.identityHashCode(w));
-	
+	System.out.println("hii");
 }
+
 }
